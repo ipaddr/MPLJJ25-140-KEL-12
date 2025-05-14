22a23ea693667743551c7e00c6ea368a7a793f66
Syauqi Revo Mardian <revosyauqi1@gmail.com>
2025-05-14 17:34:30 +0700

    Backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { sendOtp, verifyOtp, updateUserProfile, logoutUser, deleteUser, getUserProfile, } = require('./controllers/authController');
-const profileRoute = require('./routes/profileRoute');
+const { sendOtp, verifyOtp, logoutUser, deleteUser, } = require('./controllers/authController');
 const app = express();
 
 // Middleware untuk parsing JSON request body
 app.use(bodyParser.json());
-
-// Menggunakan routing untuk profil pengguna
-app.use('/api', profileRoute);
 
 // Endpoint untuk mengirim OTP
 app.post('/send-otp', sendOtp);
@@ -16,19 +12,12 @@ app.post('/send-otp', sendOtp);
 // Endpoint untuk verifikasi OTP
 app.post('/verify-otp', verifyOtp);
 
-// Endpoint untuk update profil pengguna
-app.post('/updateUserProfile', updateUserProfile);
 
 // Endpoint untuk logout pengguna
 app.post('/logout', logoutUser);
 
 // Endpoint untuk menghapus pengguna
 app.delete('/delete-user', deleteUser);
-
-// Endpoint untuk mengambil profil pengguna
-app.get('/user-profile/:uid', getUserProfile);
-
-
 
 // Menjalankan server
 const PORT = process.env.PORT || 5000;
